0c82fb125e938f74d3df7e64063459c911575b05
Igor Olkhov <smartestbox@gmail.com>
2023-03-02 13:53:01 +0100

    Add standard logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import styles from './App.module.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [countValue, setCountValue] = useState<number>(0)
+    const handleIncCount = () => {
+        if(countValue < 5) {
+            setCountValue(countValue + 1)
+        }
+    }
+    const handleResetCount = () => {
+        setCountValue(0)
+    }
+
+
+    return (
+        <div className={styles.App}>
+            <div className={styles.container}>
+                <div className={styles.display}>
+                    <span>max value:</span>
+                    <input type="number"/>
+                    <span>start value:</span>
+                    <input type="number"/>
+                </div>
+                <div className={styles.buttons}>
+                    <button className={styles.btn}>
+                        set
+                    </button>
+                </div>
+            </div>
+            <div className={styles.container}>
+                <div className={styles.display}>
+                    <span className={countValue < 5 ? styles.countValue : `${styles.countValue} ${styles.redText}`}>
+                        {countValue}
+                    </span>
+                </div>
+                <div className={styles.buttons}>
+                    <button className={countValue < 5 ? styles.btn : styles.btnDisabled}
+                            onClick={handleIncCount}
+                            disabled={countValue >=5}
+                    >
+                        inc
+                    </button>
+                    <button
+                        className={styles.btn}
+                        onClick={handleResetCount}
+                    >
+                        reset
+                    </button>
+                </div>
+
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
